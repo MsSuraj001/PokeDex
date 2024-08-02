1891733e92784ab51d53b868d041fe7e86177260
MsSuraj001 <milukumar225@gmail.com>
2024-08-02 18:32:11 +0530

    learn the handle api with react

---
+++ src/Components/Pokedex/Pokedex.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Search from '../Search/Search'
+import './Pokedex.css';
+import PokemonList from '../PokemonList/PokemonList';
+
+function Pokedex() {
+  return (
+    <div className='pokedex-wraper'>
+      <h1 id='pokedex-heading'>Pokedex</h1>
+      <Search />
+      <PokemonList />
+    </div>
+  )
+}
+
+export default Pokedex
---
+++ src/Components/PokemonList/PokemonList.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './PokemonList.css';
+import Pokemon from '../Pokemon/Pokemon';
+
+function PokemonList() {
+
+    const [pokemonList, setpokemonList] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon'
+
+    async function downloadPokemons(){
+        const response = await axios.get(POKEDEX_URL);
+        // console.log(response.data);   // next or previews url in this object
+        const pokemonResult = response.data.results
+        // console.log(pokemonResult);
+        const pokemonResultPromise = pokemonResult.map( (pokemon) => axios.get(pokemon.url) )
+        // console.log(pokemonResultPromise);
+        
+        const pokemonData = await axios.all(pokemonResultPromise)
+        // console.log(pokemonData);
+
+        const pokeListResult = pokemonData.map((pokeData) => {
+            const pokemon = pokeData.data;
+            // console.log(pokemon);
+            return {
+                id : pokemon.id,
+                name: pokemon.name, 
+                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny, 
+                types : pokemon.types
+            }
+        })
+        console.log(pokeListResult);
+        setpokemonList(pokeListResult)
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        downloadPokemons()
+    }, [])
+
+  return (
+    <div className='pokemon-list-wrapper'>
+      <div>List of pokemons</div>
+      {(isLoading) ? 'Loading....' : 
+        pokemonList.map( (p)=>  <Pokemon name={p.name} image={p.image} key={p.id} /> )
+      }
+    </div>
+  )
+}
+
+export default PokemonList
